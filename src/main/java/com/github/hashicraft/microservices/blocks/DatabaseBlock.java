252848456686233a7b1249c14175163787966e94
Nic Jackson <jackson.nic@gmail.com>
2023-09-07 14:05:05 +0100

    Refactor, add capability to configure using environment variables

--- src/main/java/com/github/hashicraft/microservices/blocks/DatabaseBlock.java
+++ src/main/java/com/github/hashicraft/microservices/blocks/DatabaseBlock.java
@@ -2,17 +2,29 @@ package com.github.hashicraft.microservices.blocks;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.hashicraft.microservices.MicroservicesMod;
+import com.github.hashicraft.microservices.environment.Env;
 import com.github.hashicraft.microservices.events.DatabaseBlockClicked;
 import com.github.hashicraft.microservices.events.Messages;
 import com.github.hashicraft.stateful.blocks.StatefulBlock;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
@@ -20,6 +32,9 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
@@ -34,10 +49,20 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.tick.TickPriority;
 
 public class DatabaseBlock extends StatefulBlock {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseBlock.class);
+
   public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
   public static final BooleanProperty POWERED = Properties.POWERED;
+
+  // keeps a map of registered database blocks so we can check for updates
+  // on server tick
+  private static HashMap<BlockPos, Boolean> DATABASES = new HashMap<BlockPos, Boolean>();
+  // background thread service
+  private static ExecutorService service = new ThreadPoolExecutor(4, 1000, 0L, TimeUnit.MILLISECONDS,
+      new LinkedBlockingQueue<Runnable>());
 
   public DatabaseBlock(Settings settings) {
     super(settings);
@@ -119,5 +144,108 @@ public class DatabaseBlock extends StatefulBlock {
 
     BlockState newState = state.with(POWERED, false);
     world.setBlockState(pos, newState, Block.NOTIFY_ALL);
+  }
+
+  // register this class to listen to server play networkiing events
+  public static void registerEvents() {
+    ServerPlayNetworking.registerGlobalReceiver(Messages.DATABASE_BLOCK_REGISTER,
+        (MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf,
+            PacketSender responseSender) -> {
+          BlockPos pos = buf.readBlockPos();
+          handleBlockRegister(pos);
+        });
+
+    ServerPlayNetworking.registerGlobalReceiver(Messages.DATABASE_BLOCK_REMOVE,
+        (MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf,
+            PacketSender responseSender) -> {
+          BlockPos pos = buf.readBlockPos();
+          handleBlockRemove(pos);
+
+        });
+
+    ServerTickEvents.START_SERVER_TICK.register((MinecraftServer server) -> {
+      server.execute(() -> {
+        handleServerTick(server.getOverworld());
+      });
+    });
+
+  }
+
+  public static void handleBlockRegister(BlockPos pos) {
+    MicroservicesMod.LOGGER.info("Received register_database message {}", pos);
+    if (!DATABASES.containsKey(pos)) {
+      DATABASES.put(pos, false);
+    }
+  }
+
+  public static void handleBlockRemove(BlockPos pos) {
+    MicroservicesMod.LOGGER.info("Received remove_database message {}", pos);
+    if (DATABASES.containsKey(pos)) {
+      DATABASES.remove(pos);
+    }
+  }
+
+  public static void handleServerTick(ServerWorld world) {
+    // check if the block is powered
+    for (Entry<BlockPos, Boolean> entry : DATABASES.entrySet()) {
+      BlockPos pos = entry.getKey();
+      DatabaseBlockEntity blockEntity = (DatabaseBlockEntity) world.getBlockEntity(pos);
+      int power = world.getReceivedRedstonePower(pos);
+
+      if (power > 0 && !entry.getValue()) {
+        MicroservicesMod.LOGGER.info("block {} power on {}", entry.getKey(), power);
+        DATABASES.put(pos, true);
+
+        executeDBQuery(world, blockEntity);
+      }
+
+      // when power is off reset the block
+      if (power == 0 && entry.getValue()) {
+        DATABASES.put(pos, false);
+      }
+    }
+  }
+
+  private static void executeDBQuery(ServerWorld world, DatabaseBlockEntity blockEntity) {
+    service.submit(() -> {
+      try {
+        LOGGER.info("Executing SQL statement {}", blockEntity.getSQLStatement());
+
+        String result = executeSQLStatement(blockEntity);
+        blockEntity.result = result;
+
+        BlockState state = blockEntity.getCachedState().with(DatabaseBlock.POWERED, true);
+        world.setBlockState(blockEntity.getPos(), state, Block.NOTIFY_ALL);
+      } catch (SQLException e) {
+        LOGGER.error("Error executing SQL statement {}", e);
+        blockEntity.result = e.getMessage();
+      }
+
+      // update the block
+      blockEntity.setPropertiesToState();
+      blockEntity.serverStateUpdated(blockEntity.serverState);
+
+      // schedule a block tick to update the block so it can disable
+      world.scheduleBlockTick(blockEntity.getPos(), MicroservicesMod.DATABASE_BLOCK, 40, TickPriority.NORMAL);
+    });
+  }
+
+  private static String executeSQLStatement(DatabaseBlockEntity blockEntity) throws SQLException {
+    // get the database details from the block entity gui
+    // we will substitute any environment variables that may be embedded in here
+    String address = Env.getValueSubstitution(blockEntity.getDbAddress());
+    String username = Env.getValueSubstitution(blockEntity.getUsername());
+    String password = Env.getValueSubstitution(blockEntity.getPassword());
+    String database = Env.getValueSubstitution(blockEntity.getDatabase());
+    String sql = Env.getValueSubstitution(blockEntity.getSQLStatement());
+
+    // execute the SQL statement
+    Connection conn = DriverManager.getConnection(String.format("jdbc:postgresql://%s/%s", address, database),
+        username, password);
+    Statement st = conn.createStatement();
+    st.execute(sql);
+    st.close();
+
+    return "success";
   }
 }
---
+++ src/main/java/com/github/hashicraft/microservices/environment/Env.java
@@ -0,0 +1,49 @@
+package com.github.hashicraft.microservices.environment;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.github.hashicraft.microservices.MicroservicesMod;
+
+public class Env {
+  // This function attempts to replace the given value with a value from the
+  // environment. Environment variables must be named "HASHICRAFT_key", the
+  // substitution syntax allows the user to specify the key as "${{env.key}}" in
+  // their strings.
+  //
+  // If no environment substitution is found, the original value is returned.
+  public static String getValueSubstitution(String value) {
+    return replaceInString(value);
+  }
+
+  private static String replaceInString(String in) {
+    Pattern pattern = Pattern.compile("(\\$\\{\\{env\\.(.+?)\\}\\})");
+    Matcher matcher = pattern.matcher(in);
+    Map<String, String> env = System.getenv();
+
+    String out = in;
+    // check all occurance
+    while (matcher.find()) {
+      // expr is the full match ${{env.[name]}}
+      // and the text to be replaced with the env var
+      String expr = matcher.group(1);
+
+      // name is the name part of ${{env.[name]}}
+      // and should match an item in projectorEnv
+      String name = matcher.group(2);
+
+      try {
+        String replacement = env.get(MicroservicesMod.MODID.toUpperCase(Locale.ENGLISH) + "_" + name);
+        if (replacement != null && !replacement.isEmpty()) {
+          out = out.replace(expr, replacement);
+        }
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    return out;
+  }
+}
