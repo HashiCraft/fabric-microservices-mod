fcb8584c03043d38d3711960b958bba5a77c6cb1
Nic Jackson <jackson.nic@gmail.com>
2025-05-31 16:51:49 +0100

    remove rust build

--- wasm_examples/rust/target/wasm32-wasi/debug/deps/module.d
+++
@@ -1,6 +0,0 @@
-/mnt/c/Users/jacks/java/fabric-wasm-mod/examples/rust/target/wasm32-wasi/debug/deps/module.wasm: src/lib.rs src/abi.rs
-
-/mnt/c/Users/jacks/java/fabric-wasm-mod/examples/rust/target/wasm32-wasi/debug/deps/module.d: src/lib.rs src/abi.rs
-
-src/lib.rs:
-src/abi.rs:
